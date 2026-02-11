ba8554a7b0098a3d3908c685933a27183b707149
victoriauglyar21 <victoriauglyar21@gmail.com>
2026-02-11 14:42:58 -0700

    updated favicon

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.cc91ut32b88"
+    "revision": "0.kr8ucm1ueqg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
